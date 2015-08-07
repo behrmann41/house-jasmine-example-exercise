65fcc3929898f540b18f4e329a7afcb9be5059f4
Ian Behrmann <ianjbehrmann@gmail.com>
2015-08-07 11:50:07 -0600

    text example completed

--- house.js
+++ house.js
@@ -1,6 +1,17 @@
-function House(numberofDoors){
-    this.numberofDoors = numberofDoors;
-
+function House(numberOfDoors, numberOfWindows){
+    this.numberOfDoors = numberOfDoors;
+    this.numberOfWindows = numberOfWindows;
+    this.toString = function(){
+        if((numberOfDoors > 1 && numberOfWindows > 1) || (numberOfDoors < 1 && numberOfWindows < 1)){
+            return "The house has " + numberOfDoors + " doors and " + numberOfWindows + " windows"
+        }else if (numberOfDoors === 1 && numberOfWindows === 1){
+          return "The house has " + numberOfDoors + " door and " + numberOfWindows + " window"
+        }else if (numberOfDoors === 1 && numberOfWindows > 1){
+          return "The house has " + numberOfDoors + " door and " + numberOfWindows + " windows"
+        }else if (numberOfDoors > 1 && numberOfWindows === 1){
+          return "The house has " + numberOfDoors + " doors and " + numberOfWindows + " window"
+        }
+    };
 }
 
 module.exports = House;
