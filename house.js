7a5d4fd085d8977a793706372808244230fe24f2
Ian Behrmann <ianjbehrmann@gmail.com>
2015-08-07 13:41:46 -0600

    refactored to use ternary

--- house.js
+++ house.js
@@ -2,15 +2,9 @@ function House(numberOfDoors, numberOfWindows){
     this.numberOfDoors = numberOfDoors;
     this.numberOfWindows = numberOfWindows;
     this.toString = function(){
-        if((numberOfDoors > 1 && numberOfWindows > 1) || (numberOfDoors < 1 && numberOfWindows < 1)){
-            return "The house has " + numberOfDoors + " doors and " + numberOfWindows + " windows"
-        }else if (numberOfDoors === 1 && numberOfWindows === 1){
-          return "The house has " + numberOfDoors + " door and " + numberOfWindows + " window"
-        }else if (numberOfDoors === 1 && numberOfWindows > 1){
-          return "The house has " + numberOfDoors + " door and " + numberOfWindows + " windows"
-        }else if (numberOfDoors > 1 && numberOfWindows === 1){
-          return "The house has " + numberOfDoors + " doors and " + numberOfWindows + " window"
-        }
+        var houseString = (numberOfDoors != 1) ? "doors" : "door";
+        var windowString = (numberOfWindows != 1) ? "windows" : "window";
+            return "The house has " + numberOfDoors + " " + houseString + " and " + numberOfWindows + " " + windowString;
     };
 }
 
